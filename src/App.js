ae6676c858040a65a60959a3782948bae14b60c3
arissetyawan1 <46475403+arissetyawan1@users.noreply.github.com>
2021-10-12 13:44:32 +0700

    Edit css file and todo style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,60 @@ import "./styles.css";
 
 export default function App() {
   return (
+    // JSX file
     <div className="App">
-      <h1>Core React Concept</h1>
+      <div className="container">
+        <div className="frame">
+          <section className="header">
+            <button className="header-btn black-color">Add</button>
+            <h1 className="header-title">Todo List</h1>
+            <button className="header-btn main-red-color">Clear</button>
+          </section>
+
+          <section className="add">
+            <form action="" className="add-form">
+              <input type="text" className="add-input" />
+              <button className="add-btn black-color">Add</button>
+            </form>
+          </section>
+
+          <section className="todos">
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+            <div className="todo">
+              <span className="todo-text">Learning React Js</span>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
