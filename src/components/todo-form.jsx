0abbb37da9b3603b81cafdd3608ab316c3346654
arissetyawan1 <46475403+arissetyawan1@users.noreply.github.com>
2021-10-13 11:28:03 +0700

    Basic State #1

--- src/components/todo-form.jsx
+++ src/components/todo-form.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
 
-const TodoForm = () => {
+  const handleFormInput = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("Please, Insert value!");
+    } else {
+      alert(value);
+      addTodo(value);
+      setValue("");
+    }
+  };
+
   return (
     <section className="add">
-      <form action="" className="add-form">
-        <input type="text" className="add-input" />
+      <form action="" className="add-form" onSubmit={handleFormInput}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn black-color">Add</button>
       </form>
     </section>
   );
 };
 
+TodoForm.PropTypes = {
+  addTodo: PropTypes.func.isRequired
+};
 export default TodoForm;
