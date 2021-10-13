ec01420397a391850fa4b83b9a879c4647d97126
arissetyawan1 <46475403+arissetyawan1@users.noreply.github.com>
2021-10-13 11:47:56 +0700

    Basic React Hook useState #2

--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 import Todo from "./todo";
 
-const Todos = () => {
-  const todo = [
-    {
-      text: "Belajar React 1"
-    },
-    {
-      text: "Belajar Component React"
-    },
-    {
-      text: "Belajar Layouting React"
-    },
-    {
-      text: "Belajar State React"
-    },
-    {
-      text: "Belajar Stateless React"
-    }
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
-      {todo.map((index) => {
+      {todos.map((index) => {
         return <Todo text={index.text} />;
       })}
     </section>
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,9 +1,24 @@
+// import state
+import { useState } from "react";
 import Paper from "../components/paper";
 import Header from "../components/header";
 import TodoForm from "../components/todo-form";
 import Todos from "../components/todos";
-
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Belajar React 1"
+    },
+    {
+      text: "Belajar Component React"
+    },
+    {
+      text: "Belajar Layouting React"
+    },
+    {
+      text: "Belajar State React"
+    }
+  ]);
   return (
     // JSX file
     <div className="App">
@@ -15,7 +30,7 @@ const TodoList = () => {
         <TodoForm />
 
         {/* Todos list */}
-        <Todos />
+        <Todos todos={todos} />
       </Paper>
     </div>
   );
